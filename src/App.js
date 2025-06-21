275be3a924b54aabe702a0103f737429f10ee2d4
Tord Falch <tordfalch@hotmail.com>
2025-06-21 16:45:40 +0200

    Seperated Tableview into own component

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
 import './App.css';
-import Table from './components/Table';
-import Controlpanel from './components/Controlpanel';
-import Tableinfo from './components/Tableinfo';
-import * as tableController from "./controllers/tableController"
+import { AuthContext } from './context/AuthContext';
 import {useState} from 'react';
-import Newtablemodal from './components/Newtablemodal';
+
+import Tableview from './components/Tableview';
 
 function App() {
 
-  const [tableList, setTableList] = useState(tableController.getTables())
-  const [floors, setFloors] = useState(tableController.getFloors())
-  const [isEdit, setIsEdit] = useState(false);
-  const [activeTable, setActiveTable] = useState();
-  const [activeFloor, setActiveFloor] = useState("0");
+
+  const [session, setSesssion] = useState(null);
 
   return (
-    <div className="App">
+    <AuthContext value={session}>
+      <Tableview></Tableview>
+    </AuthContext>
 
-      <div>
-        <Controlpanel 
-        setTableList={setTableList}
-        tableList={tableList} 
-        setIsEdit={setIsEdit}
-        isEdit={isEdit}
-        floors={floors}
-        setFloors={setFloors}
-        setActiveFloor={setActiveFloor}
-        activeFloor={activeFloor}
-        ></Controlpanel>
-        
-        <div className='tableContainer'>
-          {tableList
-          ? tableList.map((table) => {
-            if(table.floor === activeFloor){
-              return <Table 
-              key={table.id} 
-              xPos={table.x} 
-              yPos={table.y} 
-              isEdit={isEdit}
-              tableList={tableList}
-              setTableList={setTableList}
-              table={table}
-              setActiveTable={()=>{setActiveTable(table)}}
-              activeTable={activeTable}
-              ></Table>
-            }else{return null}
-
-            })
-          : <></>
-          }
-        </div>
-      </div>
-      <Tableinfo activeTable={activeTable}></Tableinfo>
-      <Newtablemodal setTableList={setTableList} tableList={tableList} activeFloor={activeFloor}></Newtablemodal>    
-    </div>
   );
 }
 
