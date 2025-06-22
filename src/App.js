1aeefd74b8605befc8032d44e62b65116fd5b436
Tord Falch <tordfalch@hotmail.com>
2025-06-22 09:57:55 +0200

    Added basic styling to loginpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 
+import './App.css';
 import Tableview from './components/Tableview';
 import SignIn from './components/Signin';
 import { BrowserRouter, Routes, Route } from 'react-router';
@@ -9,18 +9,17 @@ import { AuthContextProvider } from './context/AuthContext';
 
 
 
+
 function App() {
 
   return (
     <AuthContextProvider>
       <BrowserRouter>
-
         <Routes>
           <Route path='/' element={<PrivateRoute><Tableview /></PrivateRoute>}></Route>
           <Route path='/signin' element={<SignIn />}></Route>
           <Route path='/register' element={<Register />}></Route>
         </Routes>
-
       </BrowserRouter>
     </AuthContextProvider>
 
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -6,12 +6,14 @@ import { useNavigate } from 'react-router';
 function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
   const { session, signInUser } = UserAuth()
 
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
     try {
       const result = await signInUser(email, password);
       if (result.success) {
@@ -25,33 +27,70 @@ function SignIn() {
 
   };
 
+  const style = {
+    backgroundColor: 'black',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+  }
+
+  const formStyle = {
+    width: '20rem',
+    height: '20rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    border: '3px solid white',
+    borderRadius: '20px',
+    backgroundColor: 'grey',
+  }
+
+  const buttonStyle = {
+    width:'100px',
+    border: '2px solid white',
+    borderRadius: '20px',
+    marginTop: '10px',
+  }
+
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <h2>Sign In</h2>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Sign In</button>
-      </form>
-    </>
+      <div style={style}>
+        {loading
+          ? <h1>Loading........</h1>
+          : <form 
+              style={formStyle}
+              onSubmit={handleSubmit}
+            >
+            <h2>Sign In</h2>
+            <div>
+              <input
+              className='mt-10'
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder='Email'
+                required
+              />
+            </div>
+            <div>
+              <input
+              className='mt-10'
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder='Password'
+                required
+              />
+            </div>
+            <button type="submit" style={buttonStyle}>Sign In</button>
+          </form>
+        }
+      </div>
 
   );
 }
