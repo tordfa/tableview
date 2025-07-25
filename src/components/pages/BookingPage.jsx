ccfff3ab034d641d276b8705bf49ef4f03aecc44
Tord Falch <tordfalch@hotmail.com>
2025-07-25 18:38:47 +0200

    Starting work on multi step form for booking

--- src/components/pages/BookingPage.jsx
+++ src/components/pages/BookingPage.jsx
@@ -5,15 +5,19 @@ import { DatePicker } from '../DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import Button from '@mui/material/Button';
 import dayjs from "dayjs";
+
+
+//STEP 1: Select Number of Guest
+//STEP 2: Chose date and Time
+//STEP 3: Enter contact details.
 
 export const BookingPage = () => {
     const [isLoading, setIsLoading] = useState(false);
-    const [bookingSlots, setBookingSlots] = useState([1, 2, 3, 4]);
-
+    const [bookingSlots, setBookingSlots] = useState([]);
 
     let params = useParams();
-    let navigate = useNavigate()
 
     async function getDateBookings(timestamp) {
         const chosenDate = new Date(timestamp);
@@ -50,13 +54,18 @@ export const BookingPage = () => {
                             <StaticDatePicker
                                 minDate={dayjs()}
                                 onChange={getDateBookings}
+                                slotProps={{
+                                    actionBar: {
+                                        actions: [], // This removes all action buttons, including "OK"
+                                    },
+                                }}
                             ></StaticDatePicker>
                         </LocalizationProvider>
-                        <div className="bg-red-500 w-64 pt-12 grid grid-cols-2">
+                        <div className="border w-64 p-6 grid grid-cols-2 gap-2">
                             {bookingSlots.map(slot => (
-                                slot.is_available 
-                                    ? <h1>{slot.booking_time}</h1> 
-                                    : <h1 className="text-blue-500">{slot.booking_time}</h1>
+                                slot.is_available
+                                    ? <Button variant="outlined" size="medium">{slot.booking_time}</Button>
+                                    : <Button disabled variant="outlined" size="medium">{slot.booking_time}</Button>
                             ))}
                         </div>
                     </div>
@@ -65,5 +74,25 @@ export const BookingPage = () => {
             }
         </>
 
+    )
+}
+
+
+export const GuestStep = () => {
+    return (
+        <h1>Select number of guests: </h1>
+    )
+}
+
+export const ChoseDateTimeStep = () => {
+    return (
+        <h1>Chose Date and Time</h1>
+    )
+}
+
+
+export const ContactDetailsStep = () => {
+    return (
+        <h1>Enter contact details!</h1>
     )
 }
